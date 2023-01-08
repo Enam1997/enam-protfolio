19ebef124810c36b78b91334bca96deac6d3460a
Enam <mdenamahmedchowdhury@gmail.com>
2023-01-08 15:49:45 +0600

    Protfolio section complete

--- src/component/protfolio/Protfolio.jsx
+++ src/component/protfolio/Protfolio.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
 import "./protfolio.css"
+import Menu from './Menu';
+import { useState } from 'react';
 
 const Protfolio = () => {
+
+  const [items, setItems] = useState(Menu);
+  const filterItem = (categoryItem) =>{
+
+    const updatedItems = Menu.filter(
+      (curElem) => {
+        return curElem.category===categoryItem;
+      }
+    )
+
+    setItems(updatedItems);
+  }
+
+
   return (
-    <div>protfolio</div>
+    <section className="work container section">
+
+      <h2 className="section__title">
+        Recent Works
+      </h2>
+
+      <div className="work__filters">
+        <span className="work__item"  onClick={() => setItems(Menu)}>Everything</span>
+        <span className="work__item" onClick={() => filterItem
+        ("Creative")}>Creative</span>
+        <span className="work__item" onClick={() => filterItem
+        ("Art")}>Art</span>
+        <span className="work__item" onClick={() => filterItem
+        ("Design")}>Design</span>
+        <span className="work__item" onClick={() => filterItem
+        ("Branding")}>Branding</span>
+
+      </div>
+
+      <div className="work__container grid">
+
+        {
+          items.map(
+            (elem) => {
+              const{id, image, title, category} = elem;
+              return(
+                <div className="work__card" key={id}>
+                  <div className="work__thumbnail">
+                    <img src={image} alt="" className="work__img" />
+                    <div className="work__mask"></div>
+                  </div>
+                  <span className="work__category">{category}</span>
+                  <h3 className="work__title">{title}</h3>
+                  <a href="" className="work__button">
+                    <i className="icon-link work__button-icon"></i>
+                  </a>
+                </div>
+              )
+            }
+          )
+        }
+
+      </div>
+
+    </section>
   )
 }
 
